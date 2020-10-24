1f4d14d75e91c799357e8c1c6031d9b6f5390569
beanini <beashuker@gmail.com>
2020-10-23 22:40:52 -0400

    Added lsball

---
+++ src/commands/FortuneCommand.ts
@@ -0,0 +1,47 @@
+import { Message, MessageEmbed } from 'discord.js'
+
+import { Category, Command } from '../Command'
+import { Ichika } from '../Ichika'
+
+export const FortuneCommand = new (class extends Command {
+
+  public name = '8ball'
+  public category = Category.Fun
+  public description = 'Let Ichika predict its thoughts on a question!'
+  public aliases = ['fortune']
+
+  
+  public async run(ichika: Ichika, message: Message, args: string[]) {
+   let question = args.join(' ')
+    if (!question) return message.channel.send(`Please provide a question for me to reply with.`);
+    
+    let responses = [
+      "Without a doubt.",
+      "Yes, definitely",
+      "Most likely",
+      "Outlook good",
+      "Yes!",
+      "Reply hazy, try again",
+      "Ask again later",
+      "Can't tell you now",
+      "I can't predict it",
+      "Don't count on it",
+      "My sources say no",
+      "Outlook not so good",
+      "Absolutely not.",
+      "Very doubtful"
+  ]
+
+    let response = responses[Math.floor(Math.random() * responses.length)];
+    const user = message.author;
+
+    message.channel.send(new MessageEmbed()
+      .setTitle('`🎱` 8 Ball Fortune')
+      .setDescription('- Get Ichika\'s thoughts on a question!')
+      .setColor('#dee8eb')
+      .addField(`${user.username}'s Question:`, question)
+      .addField('My reply:', response)
+      .setFooter(`Requested by ${user.username}`, user.displayAvatarURL({ dynamic : true }))
+    )
+  }
+})
